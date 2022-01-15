51813fbabded28f9825fe2be0a81dc25f60926a3
Prysline <mayhu1993@gmail.com>
2022-01-15 23:18:55 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,35 @@
+// import package
+const express = require('express')
+const app = express()
+
+const { engine: exphbs } = require('express-handlebars')
+const methodOverride = require('method-override')
+
+// dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// import files
+const routes = require('./routes')
+
+// set environment
+const PORT = process.env.PORT || 3000
+
+// setting template engine
+app.engine('.hbs', exphbs({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+// setting static files
+app.use(express.static('public'))
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
