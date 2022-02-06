eed26e1fce8c451e15824d23b447193272bfcf99
Prysline <mayhu1993@gmail.com>
2022-02-06 15:44:56 +0800

    feat: add records route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const records = require('./modules/records')
 
 router.use('/index', home)
 router.use('/', home)
---
+++ routes/modules/records.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+router.get('/:id/edit', (req, res) => {
+
+})
+
+module.exports = router
