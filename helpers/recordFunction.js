591f46e32484a513abfcd1e689a8103117d04d97
Prysline <mayhu1993@gmail.com>
2022-02-16 23:42:44 +0800

    refactor: 將與資料庫互動相關function獨立成一支js

---
+++ helpers/recordFunction.js
@@ -0,0 +1,49 @@
+const Category = require('../models/category')
+const CATEGORY = {
+  家居物業: "home",
+  交通出行: "shuttle-van",
+  休閒娛樂: "grin-beam",
+  餐飲食品: "utensils",
+  其他: "pen"
+}
+
+module.exports = {
+  getCategoryList: () => {
+    return Category.find({}).lean().sort('id')
+  },
+
+  getCategory_idById: async (id) => {
+    return (await Category.findOne({ id }).lean())._id
+  },
+  getRecordList: async (condition) => {
+    const categories = await getCategoryList()
+    // condition: object
+    return Promise.all((await Record.find(condition).lean().sort('id')).map(async (item) => {
+      const categoryName = (await Category.findById(item.categoryId).lean()).name
+      return {
+        category: `/icons/${CATEGORY[categoryName]}-solid.svg`,
+        name: item.name,
+        date: [item.date.getFullYear(), item.date.getMonth(), item.date.getDay()].join('.'),
+        amount: item.amount,
+        _id: item._id
+      }
+    }))
+  },
+  getLatestRecordId: async () => {
+    const records = await Record.find().lean().sort('-id')
+    return records[0].id + 1
+  },
+  getTotalAmount: (records) => {
+    return records.reduce((total, item) => total + item.amount, 0)
+  },
+  getFormErrors: (name, date, category, amount) => {
+    const errors = []
+    if (!name || !date || !category || !amount) {
+      errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (amount < 0) {
+      errors.push({ message: '請輸入大於等於 0 的數字。' })
+    }
+    return errors
+  }
+}
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const dayjs = require('dayjs')
 const router = express.Router()
-const Category = require('../../models/category')
 const Record = require('../../models/record')
-const CATEGORY = {
-  家居物業: "home",
-  交通出行: "shuttle-van",
-  休閒娛樂: "grin-beam",
-  餐飲食品: "utensils",
-  其他: "pen"
-}
+const recordFunc = require('../../helpers/recordFunction')
 
 router.get('/', async (req, res, next) => {
   try {
     const userId = req.user._id
-    const categories = await getCategoryList()  // for render category
-    const records = await getRecordList({ userId })
+    const categories = await recordFunc.getCategoryList()  // for render category
+    const records = await recordFunc.getRecordList({ userId })
     const categoryName = '全部'
-    const totalAmount = getTotalAmount(records)
+    const totalAmount = recordFunc.getTotalAmount(records)
     res.render('index', { categories, records, totalAmount, categoryName })
   } catch (error) {
     next(error)
@@ -28,10 +21,10 @@ router.get('/category/:id', async (req, res) => {
   const userId = req.user._id
   const id = req.params.id
   try {
-    const categories = await getCategoryList()
+    const categories = await recordFunc.getCategoryList()
     const categoryName = categories[id - 1].name
-    const records = await getRecordList({ userId, categoryId: await getCategory_idById(id) })
-    const totalAmount = getTotalAmount(records)
+    const records = await recordFunc.getRecordList({ userId, categoryId: await recordFunc.getCategory_idById(id) })
+    const totalAmount = recordFunc.getTotalAmount(records)
     res.render('index', { categories, records, totalAmount, categoryName })
   } catch (error) {
     next(error)
@@ -39,7 +32,7 @@ router.get('/category/:id', async (req, res) => {
 })
 
 router.get('/new', async (req, res) => {
-  const categories = await getCategoryList() // for render category
+  const categories = await recordFunc.getCategoryList() // for render category
   const today = dayjs().format('YYYY-MM-DD')
   res.render('new', { categories, today })
 })
@@ -47,13 +40,13 @@ router.post('/new', async (req, res, next) => {
   console.log(req.body)
   const { name, date, category, amount } = req.body
   const userId = req.user._id
-  const errors = getFormErrors(name, date, category, amount)
+  const errors = recordFunc.getFormErrors(name, date, category, amount)
   if (errors.length) {
     return res.render('register', { errors, name, email, password, confirmPassword })
   }
   try {
-    const id = await getLatestRecordId()
     const categoryId = await getCategory_idByName(category)
+    const id = await recordFunc.getLatestRecordId()
     const record = { id, name, date, categoryId, amount, userId }
     await Record.create(record)
     req.flash('success_msg', '成功新增支出！')
@@ -64,48 +57,3 @@ router.post('/new', async (req, res, next) => {
 })
 
 module.exports = router
-
-function getCategoryList () {
-  return Category.find({}).lean().sort('id')
-}
-
-async function getCategory_idById (id) {
-  return (await Category.findOne({ id }).lean())._id
-}
-async function getCategory_idByName (name) {
-  return (await Category.findOne({ name }).lean())._id
-}
-
-async function getRecordList (condition) {
-  const categories = await getCategoryList()
-  // condition: object
-  return Promise.all((await Record.find(condition).lean().sort('id')).map(async (item) => {
-    const categoryName = (await Category.findById(item.categoryId).lean()).name
-    return {
-      category: `/icons/${CATEGORY[categoryName]}-solid.svg`,
-      name: item.name,
-      date: [item.date.getFullYear(), item.date.getMonth(), item.date.getDay()].join('.'),
-      amount: item.amount,
-      _id: item._id
-    }
-  }))
-}
-async function getLatestRecordId () {
-  const records = await Record.find().lean().sort('-id')
-  return records[0].id + 1
-}
-
-function getTotalAmount (records) {
-  return records.reduce((total, item) => total + item.amount, 0)
-}
-
-function getFormErrors (name, date, category, amount) {
-  const errors = []
-  if (!name || !date || !category || !amount) {
-    errors.push({ message: '所有欄位都是必填。' })
-  }
-  if (amount < 0) {
-    errors.push({ message: '請輸入大於等於 0 的數字。' })
-  }
-  return errors
-}
